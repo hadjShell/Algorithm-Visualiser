926c525dec0af859767a2141f4aa96f1d6064260
张家源 <davidzhang0880@gmail.com>
2023-12-13 00:17:43 +0800

    Update logo image

--- my-app/src/components/Footer.jsx
+++ my-app/src/components/Footer.jsx
@@ -1,9 +1,7 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto mt-32 w-full max-w-container px-4 sm:px-6 large:px-8">
-      <p className="mt-5 text-center text-sm leading-6 text-slate-500">
-        &copy; Hadjshell, 2023 - present
-      </p>
+    <footer>
+      <p>&copy; Hadjshell, 2023 - present</p>
     </footer>
   );
 }
