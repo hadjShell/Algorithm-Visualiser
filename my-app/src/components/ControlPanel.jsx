710016c5025cccf806c3392787fdf13004b8854f
张家源 <davidzhang0880@gmail.com>
2024-01-07 14:46:49 +0800

    Fix option color

--- my-app/src/components/ControlPanel.jsx
+++ my-app/src/components/ControlPanel.jsx
@@ -144,10 +144,18 @@ export default function ControlPanel({
           className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
           onChange={handleSpeed}
         >
-          <option value="1">1.0x</option>
-          <option value="0.5">0.5x</option>
-          <option value="2.0">2.0x</option>
-          <option value="4.0">4.0x</option>
+          <option className="text-black" value="1">
+            1.0x
+          </option>
+          <option className="text-black" value="0.5">
+            0.5x
+          </option>
+          <option className="text-black" value="2.0">
+            2.0x
+          </option>
+          <option className="text-black" value="4.0">
+            4.0x
+          </option>
         </select>
       </div>
 
